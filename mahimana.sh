1ab3ff1732fae7adb9478b2a11bc9b54d0bd26c7
Eng.Taha Shokri <tahashokri77@gmail.com>
2024-03-08 23:16:36 +0330

    Init project with 2 ability (update system - change ssh port)

---
+++ mahimana.sh
@@ -0,0 +1,192 @@
+#! /bin/bash
+
+# Color codes
+# Reset
+NC='\033[0m'       # Text Reset
+
+# Regular Colors
+Black='\033[0;30m'        # Black
+Red='\033[0;31m'          # Red
+Green='\033[0;32m'        # Green
+Yellow='\033[0;33m'       # Yellow
+Blue='\033[0;34m'         # Blue
+Purple='\033[0;35m'       # Purple
+Cyan='\033[0;36m'         # Cyan
+White='\033[0;37m'        # White
+
+# Bold
+BBlack='\033[1;30m'       # Black
+BRed='\033[1;31m'         # Red
+BGreen='\033[1;32m'       # Green
+BYellow='\033[1;33m'      # Yellow
+BBlue='\033[1;34m'        # Blue
+BPurple='\033[1;35m'      # Purple
+BCyan='\033[1;36m'        # Cyan
+BWhite='\033[1;37m'       # White
+
+# Underline
+UBlack='\033[4;30m'       # Black
+URed='\033[4;31m'         # Red
+UGreen='\033[4;32m'       # Green
+UYellow='\033[4;33m'      # Yellow
+UBlue='\033[4;34m'        # Blue
+UPurple='\033[4;35m'      # Purple
+UCyan='\033[4;36m'        # Cyan
+UWhite='\033[4;37m'       # White
+
+# Background
+On_Black='\033[40m'       # Black
+On_Red='\033[41m'         # Red
+On_Green='\033[42m'       # Green
+On_Yellow='\033[43m'      # Yellow
+On_Blue='\033[44m'        # Blue
+On_Purple='\033[45m'      # Purple
+On_Cyan='\033[46m'        # Cyan
+On_White='\033[47m'       # White
+
+# High Intensity
+IBlack='\033[0;90m'       # Black
+IRed='\033[0;91m'         # Red
+IGreen='\033[0;92m'       # Green
+IYellow='\033[0;93m'      # Yellow
+IBlue='\033[0;94m'        # Blue
+IPurple='\033[0;95m'      # Purple
+ICyan='\033[0;96m'        # Cyan
+IWhite='\033[0;97m'       # White
+
+# Bold High Intensity
+BIBlack='\033[1;90m'      # Black
+BIRed='\033[1;91m'        # Red
+BIGreen='\033[1;92m'      # Green
+BIYellow='\033[1;93m'     # Yellow
+BIBlue='\033[1;94m'       # Blue
+BIPurple='\033[1;95m'     # Purple
+BICyan='\033[1;96m'       # Cyan
+BIWhite='\033[1;97m'      # White
+
+# High Intensity backgrounds
+On_IBlack='\033[0;100m'   # Black
+On_IRed='\033[0;101m'     # Red
+On_IGreen='\033[0;102m'   # Green
+On_IYellow='\033[0;103m'  # Yellow
+On_IBlue='\033[0;104m'    # Blue
+On_IPurple='\033[0;105m'  # Purple
+On_ICyan='\033[0;106m'    # Cyan
+On_IWhite='\033[0;107m'   # White
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+    echo -e "${Red}This script must be run as root!${NC}"
+    exit 1
+fi
+
+# Define a function to display my logo
+displayLogo() {
+    printf "${Green}"
+    cat << "EOF"
+  __  __       _     _ __  __                   
+ |  \/  |     | |   (_)  \/  |                  
+ | \  / | __ _| |__  _| \  / | __ _ _ __   __ _ 
+ | |\/| |/ _` | '_ \| | |\/| |/ _` | '_ \ / _` |
+ | |  | | (_| | | | | | |  | | (_| | | | | (_| |
+ |_|  |_|\__,_|_| |_|_|_|  |_|\__,_|_| |_|\__,_|
+                                                
+                                                
+EOF
+    printf "${NC}"
+}
+
+# Update and upgrade the system
+updateAndUpgrade() {
+    printf "${Blue} 🚀 Starting Update and upgrade the system ... ${NC} \n";
+    {
+        sudo apt-get update > /dev/null 2>&1 && sudo apt-get upgrade -y > /dev/null 2>&1;
+        printf "${Green} 🎉 Update and Upgrade the system is complete ${NC} \n";
+        # wait 5 secound
+        sleep 5;
+        main;
+    } || {
+        printf "${RED}❌ An error occurred while updating the system ${NC}\n";
+        exit 1;
+    }
+}
+
+# Find SSH port and return
+FindSSHPort() {
+    # Check if ssh is installed
+    if command -v ssh &> /dev/null; then
+        # Find port in sshd_config
+        port=$(sudo grep "^Port" /etc/ssh/sshd_config | awk '{print $2}')
+        echo "$port"
+    else
+        echo "SSH is not installed"
+    fi
+}
+
+# Change SSh port
+changeSSHPort() {
+    {
+        # Check ssh and sshd is installed
+        sudo dpkg -s ssh > /dev/null 2>&1 || {
+            printf "${Blue} 🚀 Starting Install SSH ... ${NC} \n";
+            sudo apt-get install -y ssh > /dev/null 2>&1;
+            printf "${Green} 🎉 Install SSH is complete ${NC} \n";
+            }
+        read -p "Enter the new SSH port: " new_port
+        printf "${Blue} 🚀 Starting Change SSH port ... ${NC} \n";
+        # Find old port in sshd_config
+        old_port=$(sudo grep "^Port" /etc/ssh/sshd_config | awk '{print $2}')
+        # Replace old port with new port in sshd_config
+        sudo find /etc/ssh/sshd_config -type f -exec sed -i "s/^Port .*/Port $new_port/g" {} \;
+        printf "${Green} 🎉 Change SSH port is complete ${NC} \n";
+        sudo service ssh restart > /dev/null 2>&1;
+        printf "${Green} 🎉 SSH service is restarted ${NC} \n";
+        printf "${Green} 🎉 SSH port is changed to $new_port ${NC} \n";
+        # check if ufw is installed
+        if command -v ufw &> /dev/null; then
+            printf "${Blue} 🚀 Starting Close Firewall for old port... ${NC} \n";
+            sudo ufw deny $old_port > /dev/null 2>&1;
+            printf "${Green} 🎉 Firewall is closed sucessfully ${NC} \n";
+            printf "${Blue} 🚀 Starting Open Firewall ... ${NC} \n";
+            sudo ufw allow $new_port > /dev/null 2>&1;
+            printf "${Green} 🎉 Firewall is opened sucessfully ${NC} \n";
+        fi
+        # wait 5 secound
+        sleep 5;
+        main;
+
+    } || {
+        printf "${RED}❌ An error occurred while changing SSH port ${NC}\n";
+        exit 1;
+    }
+}
+
+# Main
+main() {
+    clear
+    displayLogo
+
+    printf "${Yellow}Welcome to MahiMana script - a server tool!${NC}\n"
+    printf "${Yellow}-------------------------------------------${NC}\n"
+    printf "${Green}Choose an option:${NC}\n"
+    printf "${Cyan}1. Update and upgrade the system${NC}\n"
+    printf "${Cyan}2. Change SSH port ${Purple}Current port is $(FindSSHPort) ${NC}\n"
+
+    read -p "Enter your choice: " choice
+
+    case $choice in
+        1)
+            updateAndUpgrade
+            ;;
+        2)
+            changeSSHPort
+            ;;
+        *)
+            printf "${Red}Invalid choice. Exiting.${NC}\n"
+            exit 1
+            ;;
+    esac
+}
+
+#Execute the main function
+main
